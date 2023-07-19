7f8705d15d96764634efe0bc5e830e035d297f21
kentlius <kent.delatinos@gmail.com>
2023-07-19 19:23:42 +0700

    feat: add animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,21 @@ module.exports = {
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "1s ease-out 0s 1 fade-in-down",
+      },
     },
   },
   plugins: [],
