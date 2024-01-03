f31a82cf85b6e20cc9d447bc997baed220e56391
kentlius <kent.delatinos@gmail.com>
2024-01-03 21:28:39 +0700

    chore: update deps

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
